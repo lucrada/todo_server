531ceaf8271ead7b9aa1bbbc4ff4b00295897df0
Lucrada <iamdracula.dracula@gmail.com>
2024-02-02 12:16:58 +0530

    added comments

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ categories = [
     { id: 3, title: 'cat 3' },
 ];
 
-app.get('/api/todo/:categoryId', (req, res) => {
+app.get('/api/todo/:categoryId', (req, res) => { // List all the todo items in a given category
     let categoryId = parseInt(req.params.categoryId);
     let todos = todo.filter(item => item.category_id === categoryId);
     if (todos.length == 0) {
@@ -28,25 +28,25 @@ app.get('/api/todo/:categoryId', (req, res) => {
     res.status(200).send({ todos });
 });
 
-app.get('/api/categories', (_, res) => {
+app.get('/api/categories', (_, res) => { // List all the categories
     res.status(200).send({categories});
 });
 
-app.post('/api/category/add', (req, res) => {
+app.post('/api/category/add', (req, res) => { // Add new category item
     let newItem = req.body;
     newItem = { id: categories.length + 1, ...newItem };
     categories.push(newItem);
     res.status(200).send({ message: 'New category added successfully' });
 });
 
-app.post('/api/todo/add', (req, res) => {
+app.post('/api/todo/add', (req, res) => { // Add new todo item
     let newItem = req.body;
     newItem = { id: todo.length + 1, ...newItem };
     todo.push(newItem);
     res.status(200).send({ message: 'New todo added successfully' });
 });
 
-app.delete('/api/todo/delete/:id', (req, res) => {
+app.delete('/api/todo/delete/:id', (req, res) => { // Delete todo item with given id
     let todoId = parseInt(req.params.id), len = todo.length;
     todo = todo.filter(item => item.id != todoId);
     if (len === todo.length) {
@@ -56,7 +56,7 @@ app.delete('/api/todo/delete/:id', (req, res) => {
     res.status(200).send({ message: `Todo with id: ${todoId} is deleted successfully` });
 });
 
-app.put('/api/todo/toggle_status/:id', (req, res) => {
+app.put('/api/todo/toggle_status/:id', (req, res) => { // Toggle finished status of a todo item with given id
     let todoId = parseInt(req.params.id);
     todo.map(item => {
         if (item.id == todoId) {
